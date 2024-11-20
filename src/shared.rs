24676151f37f73e764599bed32cbdcaa288d3c74
Ivan Li <ivanli2048@gmail.com>
2024-11-20 23:47:46 +0800

    chore: FAN_MINIMUM_DUTY_CYCLE = 0.2
    Signed-off-by: Ivan Li <ivanli2048@gmail.com>

--- src/shared.rs
+++ src/shared.rs
@@ -3,6 +3,6 @@ pub const OTP_SHUTDOWN_TEMP: f64 = 60.0;
 pub const OTP_HYSTERESIS_TEMP: f64 = 50.0;
 pub const OTP_THERMOREGULATION_TEMP: f64 = 40.0;
 
-pub const FAN_MINIMUM_DUTY_CYCLE: f64 = 0.15;
+pub const FAN_MINIMUM_DUTY_CYCLE: f64 = 0.2;
 
 pub const ADC_DIVIDER: f64 = 22.1 / (82.0 + 22.1); // VIN--[ 82 ]--ADC--[22.1]--GND
